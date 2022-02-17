05c09946f0d18e2a9ad360d00473c4ba29ce0fb5
daiisuukee <1278578896@qq.com>
2022-02-17 19:09:08 +0800

    大更新~~~~稳定版！

--- 我是可爱的小jio本/hit-sauRelease版.js
+++ 我是可爱的小jio本/hit-sauRelease版.js
@@ -1,21 +1,17 @@
 /*
- * @Author: your name
- * @Date: 2022-02-16 19:21:51
- * @LastEditTime: 2022-02-16 21:02:56
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \Hit-SAU\我是可爱的小jio本\hit-sau配置型 copy.js
- */
-/*
  * @Author: Daiisuukee（黛苏珂）
  * @Date: 2022-02-15 12:25:55
- * @LastEditTime: 2022-02-16 19:18:05
+ * @LastEditTime: 2022-02-17 18:45:43
  * @LastEditors: Please set LastEditors
  * @Description: 本脚本仅供学习交流使用，禁止用于商业用途，产生的一系列法律纠纷由使用者本人承担，作者不承担任何责任
  */
+// 下面的变量可以根据你的需求进行调节↓
+// 网页刷新参数（网络不好适当调节）
+var login_waiting_time = 2000; // 进入填报中心的等待时间，默认2秒，提示登录失败请适当提高值
+var fresh_cnt = 0;// 默认网页最大刷新次数为3次
+var fresh_max_time = 10000;// 默认网页等待时间为10s
 
-
-
+// 日期函数
 Date.prototype.format = function(formatStr){   
     var str = formatStr;   
     var Week = ['日','一','二','三','四','五','六'];   
@@ -46,7 +42,8 @@ function killApp(name) {
                     sleep(500);
                     log("结束运行按钮的位置")
                     log(forcedStop_location)
-                    click((forcedStop_location.left + forcedStop_location.right) / 2, (forcedStop_location.top + forcedStop_location.bottom) / 2);
+                    //click((forcedStop_location.left + forcedStop_location.right) / 2, (forcedStop_location.top + forcedStop_location.bottom) / 2);
+                    click(forcedStop_location.centerX(), forcedStop_location.centerY());
                     sleep(500);
                     for (var j = 0; j < queryStr.length; j++) {
                         if (textContains(queryStr[j]).exists()) {// 找到确认键
@@ -65,11 +62,45 @@ function killApp(name) {
     }
 }
 
+// 刷新网页
+function freashNet() {
+    fresh_cnt++;
+    if (fresh_cnt > 3) {
+        toastLog("当前网页无法加载，可能是由于网络不佳，请排除问题后重新运行打卡姬");
+        toastLog("打卡姬由于网络问题退出( >﹏<。)～");
+        engines.myEngine().forceStop();
+    }
+
+    var options = id("com.tencent.wework:id/kcn").findOnce();
+    click(options.bounds().centerX(), options.bounds().centerY());
+    sleep(500);
+    var fresh_btn_test = "";
+    var fresh_btn = text("Refresh").findOnce();
+    if (!fresh_btn.enabled()) {
+        fresh_btn = text("刷新").findOnce();
+        fresh_btn_test = "刷新"
+    } else {
+        fresh_btn_test = "Refresh";
+    }
+    
+    if(fresh_btn_test == "Refresh") {
+        click("Refresh");
+        toastLog("成功刷新网页");
+    } else if (fresh_btn_test == "刷新") {
+        click("刷新");
+        toastLog("成功刷新网页");
+    } else {
+        toastLog("未找到刷新按钮");
+    }
+    sleep(500);
+    
+}
 ///////////////////////////////main
-toastLog("本程序仅作为学习交流使用，禁止私自转发、用于商业用途！！！有bug请QQ：1278578896");
-toastLog("本程序仅作为学习交流使用，禁止私自转发、用于商业用途！！！有bug请QQ：1278578896");
-log("脚本开始运行")
+log("脚本开始运行");
+
 var language = "";
+toastLog("本程序仅作为学习交流使用，禁止私自转发、用于商业用途！！！有bug请QQ：1278578896");
+toast("本程序仅作为学习交流使用，禁止私自转发、用于商业用途！！！有bug请QQ：1278578896");
 if (launchApp("WeCom")) {// 打开企业微信
     language = "en-US";
     log("程序启动成功");
@@ -91,21 +122,51 @@ while(true) {// 顺便判断下系统语言
 }
 log("当前系统语言：")
 log (language);
-sleep(300)
+sleep(300);
+
 while(!click("c.生活服务"));
-sleep(1000)
-while(!click("师生健康信息填报"));
-while(!click("每日上报"));// 避免网卡阻塞
-var tempertuare = className("Spinner").findOnce(2);
-tempertuare.click();
-sleep(1000);
-click(580, 1061);
+sleep(login_waiting_time);
+
+var d = new Date();
+var time_begin = d.getTime();// 网页加载计时，默认超过十秒自动刷新，刷新三次结束脚本
+while(true) {
+    if (click("师生健康信息填报")) {
+        break;
+    }
+    d = new Date();
+    var now = d.getTime() - time_begin;
+    if (now > fresh_max_time) {
+        freashNet();
+        time_begin = d.getTime();
+    }
+    sleep(300);
+}
+d = new Date();
+time_begin = d.getTime();
+while(true) {
+    if (click("每日上报")) {
+        break;
+    }
+    d = new Date();
+    if (d.getTime() - time_begin > fresh_max_time) {
+        freashNet();
+        time_begin = d.getTime();
+    }
+    sleep(300);
+}
 
 var date = className("EditText").findOnce(6);
 log("下面是当前时间：")
 var time = new Date().format("yyyy-mm-dd");
 log(time);
 date.setText(time);
+sleep(500);
+
+var tempertuare = text("体温(早)*").findOnce();
+click(tempertuare.bounds().centerX(), tempertuare.bounds().centerY() + 70);
+sleep(500);
+click(device.width/2, device.height/2 - 100);
+
 
 while(!click("提交信息"));
 while(!click("确定"));
--- 我是可爱的小jio本/hit-sau配置型.js
+++ 我是可爱的小jio本/hit-sau配置型.js
@@ -1,14 +1,21 @@
 /*
  * @Author: Daiisuukee（黛苏珂）
  * @Date: 2022-02-15 12:25:55
- * @LastEditTime: 2022-02-16 21:06:45
+ * @LastEditTime: 2022-02-17 18:45:19
  * @LastEditors: Please set LastEditors
  * @Description: 本脚本仅供学习交流使用，禁止用于商业用途，产生的一系列法律纠纷由使用者本人承担，作者不承担任何责任
  */
+// 下面的变量可以根据你的需求进行调节↓
+// 解屏参数
+var pw = "114514";// ！！这里写下你的开机密码，需要纯数字！！
+var unlockTime = "1500"// 屏幕解锁预备时长，默认1.5s,如果经常无法解锁屏幕请适当提高值
+var swipeF2W = [7, 1];// 滑动手势，默认从7滑到1（详见Readme.md）
+// 网页刷新参数（网络不好适当调节）
+var login_waiting_time = 2000; // 进入填报中心的等待时间，默认2秒，提示登录失败请适当提高值
+var fresh_cnt = 0;// 默认网页最大刷新次数为3次
+var fresh_max_time = 10000;// 默认网页等待时间为10s
 
-
-var pw = "114514";// 这里写下你的开机密码，需要纯数字
-
+// 日期函数
 Date.prototype.format = function(formatStr){   
     var str = formatStr;   
     var Week = ['日','一','二','三','四','五','六'];   
@@ -39,7 +46,8 @@ function killApp(name) {
                     sleep(500);
                     log("结束运行按钮的位置")
                     log(forcedStop_location)
-                    click((forcedStop_location.left + forcedStop_location.right) / 2, (forcedStop_location.top + forcedStop_location.bottom) / 2);
+                    //click((forcedStop_location.left + forcedStop_location.right) / 2, (forcedStop_location.top + forcedStop_location.bottom) / 2);
+                    click(forcedStop_location.centerX(), forcedStop_location.centerY());
                     sleep(500);
                     for (var j = 0; j < queryStr.length; j++) {
                         if (textContains(queryStr[j]).exists()) {// 找到确认键
@@ -58,22 +66,58 @@ function killApp(name) {
     }
 }
 
-///////////////////////////////main
-log("脚本开始运行");
-sleep(1500);
-if (!device.isScreenOn()) {// 如果屏幕没亮则解锁
+// 刷新网页
+function freashNet() {
+    fresh_cnt++;
+    if (fresh_cnt > 3) {
+        toastLog("当前网页无法加载，可能是由于网络不佳，请排除问题后重新运行打卡姬");
+        toastLog("打卡姬由于网络问题退出( >﹏<。)～");
+        engines.myEngine().forceStop();
+    }
+
+    var options = id("com.tencent.wework:id/kcn").findOnce();
+    click(options.bounds().centerX(), options.bounds().centerY());
+    sleep(500);
+    var fresh_btn_test = "";
+    var fresh_btn = text("Refresh").findOnce();
+    if (!fresh_btn.enabled()) {
+        fresh_btn = text("刷新").findOnce();
+        fresh_btn_test = "刷新"
+    } else {
+        fresh_btn_test = "Refresh";
+    }
+    
+    if(fresh_btn_test == "Refresh") {
+        click("Refresh");
+        toastLog("成功刷新网页");
+    } else if (fresh_btn_test == "刷新") {
+        click("刷新");
+        toastLog("成功刷新网页");
+    } else {
+        toastLog("未找到刷新按钮");
+    }
+    sleep(500);
+    
+}
+function openPhone() {
     device.wakeUp();
     sleep(500);// 如果你的上滑输入密码界面进不去，请调高数值试试。
-    swipe(device.width/2, device.height/8*7,device.width/2,device.height/8,1000);
+    swipe(device.width/2, device.height/8*swipeF2W[0],device.width/2,device.height/8*swipeF2W[1],1000);
     sleep(1000);  
     
     for (var i = 0; i <= pw.length; i++) {
         click(pw[i]);
     }
 }
+///////////////////////////////main
+log("脚本开始运行");
+sleep(unlockTime);
+if (!device.isScreenOn()) {// 如果屏幕没亮则解锁
+    openPhone();
+}
 var language = "";
 toastLog("本程序仅作为学习交流使用，禁止私自转发、用于商业用途！！！有bug请QQ：1278578896");
-toastLog("本程序仅作为学习交流使用，禁止私自转发、用于商业用途！！！有bug请QQ：1278578896");
+toast("本程序仅作为学习交流使用，禁止私自转发、用于商业用途！！！有bug请QQ：1278578896");
 if (launchApp("WeCom")) {// 打开企业微信
     language = "en-US";
     log("程序启动成功");
@@ -95,21 +139,51 @@ while(true) {// 顺便判断下系统语言
 }
 log("当前系统语言：")
 log (language);
-sleep(300)
+sleep(300);
+
 while(!click("c.生活服务"));
-sleep(1000)
-while(!click("师生健康信息填报"));
-while(!click("每日上报"));// 避免网卡阻塞
-var tempertuare = className("Spinner").findOnce(2);
-tempertuare.click();
-sleep(1000);
-click(580, 1061);
+sleep(login_waiting_time);
+
+var d = new Date();
+var time_begin = d.getTime();// 网页加载计时，默认超过十秒自动刷新，刷新三次结束脚本
+while(true) {
+    if (click("师生健康信息填报")) {
+        break;
+    }
+    d = new Date();
+    var now = d.getTime() - time_begin;
+    if (now > fresh_max_time) {
+        freashNet();
+        time_begin = d.getTime();
+    }
+    sleep(300);
+}
+d = new Date();
+time_begin = d.getTime();
+while(true) {
+    if (click("每日上报")) {
+        break;
+    }
+    d = new Date();
+    if (d.getTime() - time_begin > fresh_max_time) {
+        freashNet();
+        time_begin = d.getTime();
+    }
+    sleep(300);
+}
 
 var date = className("EditText").findOnce(6);
 log("下面是当前时间：")
 var time = new Date().format("yyyy-mm-dd");
 log(time);
 date.setText(time);
+sleep(500);
+
+var tempertuare = text("体温(早)*").findOnce();
+click(tempertuare.bounds().centerX(), tempertuare.bounds().centerY() + 70);
+sleep(500);
+click(device.width/2, device.height/2 - 100);
+
 
 while(!click("提交信息"));
 while(!click("确定"));
